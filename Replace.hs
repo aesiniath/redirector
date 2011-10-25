a5fe0c8da3fb04d5c3348f50967be921eef0330d
Andrew Cowie <andrew@operationaldynamics.com>
2011-10-25 11:17:14 +0200

    Echo headers back to requestor on /headers

--- Replace.hs
+++ Replace.hs
@@ -4,13 +4,15 @@ import Snap.Http.Server
 import Snap.Types
 import Snap.Util.FileServe
 import Control.Applicative
-import Prelude hiding (length, appendFile, take)
+import Prelude hiding (length, appendFile, take, concat)
 import Data.ByteString.Char8
 import Data.String
 import Data.Time (formatTime, getCurrentTime, UTCTime)
 import Data.Time.Clock (getCurrentTime)
 import System.Locale (defaultTimeLocale)
 import Control.Monad.IO.Class
+import Data.Map (Map, foldWithKey)
+import Data.CaseInsensitive (CI, original)
 
 formatTimestamp :: UTCTime -> String
 formatTimestamp x = formatTime defaultTimeLocale "%a, %e %b %y %H:%M:%S.%q" x
@@ -29,8 +31,27 @@ serveTime = do
     writeBS $ append time "Z\n"
 
 
+combine :: CI ByteString -> [ByteString] -> ByteString -> ByteString
+combine k x acc = append acc $ concat [key, ": ", value, "\n"]
+    where
+        key = original k
+        value = intercalate ", " x
+
+
+join :: Headers -> ByteString
+join m = foldWithKey combine "" m
+
+
+serveHeaders :: Snap ()
+serveHeaders = do
+    req <- getRequest
+    let h = headers req
+    writeBS $ join h
+
+
 site :: Snap ()
-site = route [ ("/time", serveTime) ]
+site = route [("/time", serveTime),
+        ("/headers", serveHeaders)]
     <|> serveDirectory "content/"
 
 
