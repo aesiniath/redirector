b3b30139dc3bf1a5d6fbec2dc694a4621289ee12
Andrew Cowie <andrew@operationaldynamics.com>
2011-12-20 10:21:44 +1100

    Add Redis lookup logic

--- Redirector.hs
+++ Redirector.hs
@@ -17,16 +17,22 @@
 -- contacted through http://research.operationaldynamics.com/
 --
 
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
 
 import Snap.Http.Server
 import Snap.Core
 import Snap.Util.FileServe
 import Control.Applicative
-import qualified Data.ByteString.Char8 as Strict
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as S
+import qualified Data.ByteString.Lazy.Char8 as L
 import Data.Maybe (fromMaybe)
 import Numeric
 import Data.Char
+import Database.Redis
+import Control.Monad.Trans (liftIO)
+import Prelude hiding (catch)
+import Control.Exception
 
 --
 -- Conversion between decimal and base 62
@@ -39,16 +45,17 @@ represent x | x < 10 = chr (48 + x)
             | otherwise = '@'
 
 encode :: Int -> String
-encode x   = showIntAtBase 62 represent x ""
+encode x    = showIntAtBase 62 represent x ""
 
+
+value :: Char -> Int
+value c     | isDigit c = ord c - 48
+            | isUpper c = ord c - 65 + 10
+            | isLower c = ord c - 97 + 36
+            | otherwise = 0
 
 multiply :: Int -> Char -> Int
 multiply acc c = acc * 62 + value c
-    where
-        value c | isDigit c = ord c - 48
-        value c | isUpper c = ord c - 65 + 10
-        value c | isLower c = ord c - 97 + 36
-        value c | otherwise = 0
 
 decode :: String -> Int
 decode ss   = foldl multiply 0 ss
@@ -57,17 +64,64 @@ decode ss   = foldl multiply 0 ss
 -- Process jump hash
 --
 
-lookupHash :: String -> String
-lookupHash = undefined
+fromValue :: RedisValue -> S.ByteString
+fromValue v = case v of
+        RedisString s   -> s
+        RedisInteger i  -> S.pack $ show i
+        RedisNil        -> "" -- used to be "(nil)"
+        RedisMulti vs   -> S.intercalate "\n" $ map fromValue vs
+
+
+toKey :: S.ByteString -> L.ByteString
+toKey x = L.fromChunks [x]
+
+
+queryKey :: Server -> S.ByteString -> IO S.ByteString
+queryKey r x = catch
+        (do
+            k <- get r $ toKey x
+            return $ fromValue k)
+        (\(e :: RedisError) -> do
+            putStrLn $ show e
+            return "")
+
+
+lookupHash :: S.ByteString -> Snap S.ByteString
+lookupHash x = bracketSnap
+    (connect "localhost" 6379)
+    (disconnect)
+    (\r -> liftSnap $ liftIO $ queryKey r x)
+
 
 serveJump :: Snap ()
 serveJump = do
-    switch <- getParam "switch"
-    hash   <- getParam "hash"
-    redirect' "http://www.operationaldynamics.com/" 301
+    h <-  getParam "hash"
+    let h' = fromMaybe "" h
+    t <- lookupHash h'
+    if t == ""
+    then
+        serveNotFound
+    else
+        redirect' t 301
+
+--
+-- If a key is requested that doesn't exist, we give 404. TODO
+-- As this is probably a common case, we should serve a more
+-- interesting page.
+--
 
 serveNotFound :: Snap ()
-serveNotFound = undefined
+serveNotFound = do
+    modifyResponse $ setResponseStatus 404 "Not Found"
+    writeBS "404 Not Found"
+
+--
+-- If they request / then we send them to the corporate home page 
+--
+
+serveHome :: Snap ()
+serveHome = do
+    redirect' "http://www.operationaldynamics.com/" 302
 
 --
 -- Top level URL routing logic.
@@ -75,7 +129,8 @@ serveNotFound = undefined
 
 site :: Snap ()
 site = route
-    [("/:switch/:hash", serveJump)]
+    [("/", serveHome),
+     ("/:hash", serveJump)]
     <|> serveNotFound
 
 main :: IO ()
