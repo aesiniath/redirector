e0a41e439a0ab88aaf4143716801945be6fe5291
Andrew Cowie <andrew@operationaldynamics.com>
2012-01-04 19:30:55 +1100

    Move hash calculations to new file

--- Hashes.hs
+++ Hashes.hs
@@ -19,46 +19,39 @@
 
 {-# LANGUAGE OverloadedStrings #-}
 
-module Lookup (lookupHash, test) where
+module Hashes (encode, decode) where
 
 import qualified Data.ByteString.Char8 as S
 import qualified Data.ByteString.Lazy.Char8 as L
 import Data.Maybe (fromMaybe)
-import Database.Redis
-import Control.Monad.Trans (liftIO)
-import Control.Monad.CatchIO (MonadCatchIO, bracket)
-
+import Numeric (showIntAtBase)
+import Data.Char (isDigit, isUpper, isLower, chr, ord)
 
 --
--- Process jump hash
+-- Conversion between decimal and base 62
 --
 
-fromValue :: RedisValue -> S.ByteString
-fromValue v = case v of
-        RedisString s   -> s
-        RedisInteger i  -> S.pack $ show i
-        RedisNil        -> "" -- used to be "(nil)"
-        RedisMulti vs   -> S.intercalate "\n" $ map fromValue vs
+represent :: Int -> Char
+represent x | x < 10 = chr (48 + x)
+            | x < 36 = chr (65 + x - 10)
+            | x < 62 = chr (97 + x - 36)
+            | otherwise = '@'
+
+encode :: Int -> String
+encode x    = showIntAtBase 62 represent x ""
 
 
-toKey :: S.ByteString -> L.ByteString
-toKey x = L.fromChunks [x]
+value :: Char -> Int
+value c     | isDigit c = ord c - 48
+            | isUpper c = ord c - 65 + 10
+            | isLower c = ord c - 97 + 36
+            | otherwise = 0
 
+multiply :: Int -> Char -> Int
+multiply acc c = acc * 62 + value c
 
-queryKey :: Server -> S.ByteString -> IO S.ByteString
-queryKey r x = do
-        k <- get r key
-        return $ fromValue k
-    where
-        key = toKey $ S.append "target:" x
+decode :: String -> Int
+decode ss   = foldl multiply 0 ss
 
-
-lookupHash :: S.ByteString -> IO S.ByteString
-lookupHash x = bracket
-        (connect "localhost" 6379)
-        (disconnect)
-        (\r -> queryKey r x)
-
-
-test :: String -> IO S.ByteString
-test x = lookupHash $ S.pack x
+calculateHash :: String -> Int
+calculateHash ss = undefined
